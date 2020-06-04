d4c4c9f9b3860274153af476a6a0833f74145cff
li <1505899522@qq.com>
2020-06-04 10:24:02 +0800

    deploy

---
+++ precache-manifest.174abd747cf6d58cd9520d0557e3cdeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb42831d71b0ea91cc5",
+    "url": "/money-website/css/app.fe5cc3f7.css"
+  },
+  {
+    "revision": "685a14a49185d09744d5ab38b4d80631",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "cfb42831d71b0ea91cc5",
+    "url": "/money-website/js/app.0215f033.js"
+  },
+  {
+    "revision": "e516a4916505fbeaafe7",
+    "url": "/money-website/js/chunk-vendors.8a73c593.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
